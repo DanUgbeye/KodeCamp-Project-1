bd3691865f089c93f45d431efbf6b843b1957d66
DanUgbeye <82518175+DanUgbeye@users.noreply.github.com>
2022-04-01 12:39:03 +0100

    Made ui changes

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import GiftCards from "./components/GiftCards";
 
 function App() {
   return (
-    <div className=" w-full ">
-      <header className=" px-8 text-center shadow-md shadow-black/40 sticky top-0 left-0 right-0 text-2xl font-semibold py-4 mb-12 bg-green-500 text-white tracking-wide ">
-        GiftCards
+    <div className=" w-full min-h-[100vh] bg-[#E2D0F0] ">
+      <header className=" px-8 text-center shadow-md shadow-black/30 sticky top-0 left-0 right-0 text-3xl font-semibold py-4 mb-12 bg-[#DBC1ED] text-[#76568A] tracking-wide ">
+        GIFTCARDS
       </header>
 
       <div className=" mx-auto w-[90%] ">
--- src/components/GiftCards.jsx
+++ src/components/GiftCards.jsx
@@ -7,20 +7,20 @@ const GiftCards = () => {
   const [ cards ] = useState(allGiftCards);
 
   return (
-    <div className=" grid grid-cols-1 md:grid-cols-2 min-w-[90%] px-12 text-green-800 gap-8 ">
+    <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 w-full px-12  gap-8 ">
       {
         cards.map((card) => (
-          <div className=" mx-auto mb-4 w-full bg-green-400 rounded-md max-w-sm py-4 px-8 shadow-md shadow-black/50 " key={card.id} >
+          <div className=" mx-auto mb-4 w-full bg-[#E8D9F3] hover:bg-[#DEBEF4] rounded-md max-w-sm py-4 px-8 shadow-[0px_2px_15px_0px] hover:shadow-[0px_2px_25px_1px] hover:shadow-black/30 shadow-black/25 transition-all ease-linear duration-400 " key={card.id} >
 
-            <div className=" text-xl font-semibold text-green-800  ">{card.title}</div>
+            <div className=" text-2xl font-semibold text-[#76568A]  ">{card.title}</div>
 
-            <div className=" py-4 ">
+            <div className=" py-4 text-[#8D6F95] ">
 
-              <div className=" mb-2 ">Accepted Currencies: </div>
+              <div className=" mb-2 font-semibold text-lg ">Accepted Currencies: </div>
 
               <div id="currencies" className=" gap-4 grid grid-cols-4 ">
                 {card.currencies.map((currency) => (
-                  <div className=" bg-green-500 shadow-md shadow-black/40 rounded-full p-1 text-center " key={uuid()} >{currency}</div>
+                  <div className=" border-b border-solid border-[#8D6F95] w-fit p-1 text-center " key={uuid()} >{currency.toUpperCase()}</div>
                 ))}
               </div>
 
